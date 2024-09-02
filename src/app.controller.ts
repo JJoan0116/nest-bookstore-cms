67da4d1b1f03ae8c7b00eb617f5f2af8b20b0f9b
chenlingyu <chenlingyu@sto.com>
2024-09-02 10:45:10 +0800

    feat: add validation pipe

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  ParseIntPipe,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
+import { UserService } from './user/user.service';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Injectable(AppService)
+  // private readonly appService: AppService;
+
+  // @Injectable(UserService)
+  // private readonly userService: UserService;
+
   @Get()
-  getHello(): string {
+  getHello(
+    @Query('aa')
+    aa: string,
+  ): string {
+    console.log('11111', aa);
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { UserService } from './user/user.service';
 
 @Injectable()
 export class AppService {
+  @Inject(UserService)
+  private userService: UserService;
+
+  // @SetMetadata('aaa', 'admin')
+  // @UseGuards(AaaGuard)
+
   getHello(): string {
+    // debugger;
+    // const allUsers = this.userService.findAll();
+    // console.log('allUsers', allUsers);
     return 'Hello World!';
   }
 }
